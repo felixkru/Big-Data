1aef6832a45f5218340295670686ea9e1df1f578
l.knippenberg <lukas.knippenberg@noz-digital.de>
2024-01-31 09:13:04 +0100

    remove comments

--- src/main.py
+++ src/main.py
@@ -3,5 +3,3 @@ import fileReader
 folder_path = '../dataset/'
 analyzer = fileReader.HDF5Analyzer(folder_path)
 analyzer.analyze_files()
-combined_data = analyzer.get_combined_data()
-print(combined_data)

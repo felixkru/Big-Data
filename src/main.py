91351c8718fa39b7a850076c49c344128da1fbf9
rainyy <jan.kruse@hs-osnabrueck.de>
2024-03-02 07:37:19 +0100

    feat: added function to write data into mongoDB and changed how file_reader_h5.py packs data

--- src/main.py
+++ src/main.py
@@ -1,10 +1,16 @@
+import numpy
 import file_reader_h5
 import mongoConnection
 
+
 if __name__ == "__main__":
-    path = "../parkplatz"
+    path = "../dataset"
     analyzer = file_reader_h5.HDF5Analyzer(path)
 
     dataset = analyzer.handle_file_reader()
 
     mongoConnection.send_data_to_mongo(dataset)
+
+
+
+
--- src/mongoConnection.py
+++ src/mongoConnection.py
@@ -3,20 +3,14 @@ from pymongo.server_api import ServerApi
 
 
 def send_data_to_mongo(processed_datasets):
-    uri = "mongodb+srv://felixkruse:FXq82hcEQSH5bNmR@cluster0.bkat9hf.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+    uri = "bitte bei felix anfragen"
     client = MongoClient(uri, server_api=ServerApi('1'))
     database = client['Big-D']
-    collection = database['Measurements']
-
-    false_dataset_counter = 0
-    for item in processed_datasets:
-        if not isinstance(item, dict):
-            false_dataset_counter += 1
-    print(false_dataset_counter)
+    collection = database['raw_measurements']
 
     try:
         collection.insert_many(processed_datasets)
-        print(f'Daten erfolgreich in MongoDB eingefügt: {processed_datasets}')
+        print(f'Daten erfolgreich in MongoDB eingefügt.')
     except Exception as e:
         print(f'Fehler beim Einfügen in MongoDB: {e}')
     finally:

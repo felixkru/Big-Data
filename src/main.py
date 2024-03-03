5f6b06d95f9d43a35ba7a1fdef61ab46171c43e4
rainyy <jan.kruse@hs-osnabrueck.de>
2024-03-03 16:53:36 +0100

    feat: added read and count functions for DB queries

--- src/main.py
+++ src/main.py
@@ -3,13 +3,13 @@ import mongoConnection
 
 
 if __name__ == "__main__":
-    path = "../test"
-    analyzer = file_reader_h5.HDF5Analyzer(path)
+    path = "../dataset"
+    ##analyzer = file_reader_h5.HDF5Analyzer(path)
 
-    dataset = analyzer.handle_file_reader()
+    ##dataset = analyzer.handle_file_reader()
 
-    mongoConnection.send_data_to_mongo(dataset)
+    query_counter_result = mongoConnection.count_data_from_mongo({"region": "Africa"})
+    print(f"Soviele Datensätze entsprechen deinem Query: {query_counter_result}")
 
-
-
-
+    query_content = mongoConnection.read_data_from_mongo({"file_id": 312})
+    print(query_content)
--- src/mongoConnection.py
+++ src/mongoConnection.py
@@ -1,17 +1,54 @@
 from pymongo.mongo_client import MongoClient
 from pymongo.server_api import ServerApi
 
+uri = ""
+
 
 def send_data_to_mongo(processed_datasets):
-    uri = "bitte bei felix anfragen"
     client = MongoClient(uri, server_api=ServerApi('1'))
     database = client['Big-D']
     collection = database['raw_measurements']
-
     try:
         collection.insert_many(processed_datasets)
+
         print(f'Daten erfolgreich in MongoDB eingefügt.')
     except Exception as e:
         print(f'Fehler beim Einfügen in MongoDB: {e}')
+    finally:
+        client.close()
+
+
+def read_data_from_mongo(query=None):
+    client = MongoClient(uri, server_api=ServerApi('1'))
+    database = client['Big-D']
+    collection = database['raw_measurements']
+    query_result = []
+    if query is None:
+        query = {}
+    try:
+        for document in collection.find(query):
+            query_result.append(document)
+        print(f'Daten erfolgreich gelesen.')
+        return query_result
+    except Exception as e:
+        print(f'Fehler beim lesen der Daten: {e}')
+        return None
+    finally:
+        client.close()
+
+
+def count_data_from_mongo(query=None):
+    client = MongoClient(uri, server_api=ServerApi('1'))
+    database = client['Big-D']
+    collection = database['raw_measurements']
+    if query is None:
+        query = {}
+    try:
+        query_result = collection.count_documents(query)
+        print(f'Daten erfolgreich gezählt.')
+        return query_result
+    except Exception as e:
+        print(f'Fehler beim lesen der Daten: {e}')
+        return None
     finally:
         client.close()

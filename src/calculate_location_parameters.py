70f14833fa17696bf129bca67069c81ef73db224
l.knippenberg <lukas.knippenberg@noz-digital.de>
2024-03-06 14:02:21 +0100

    add average and median

---
+++ src/calculate_location_parameters.py
@@ -0,0 +1,27 @@
+import numpy as np
+
+
+class CalculateLocationParameters:
+
+    def __init__(self):
+        pass
+
+    @staticmethod
+    def calculate_median(data_array):
+        return np.median(data_array)
+
+    @staticmethod
+    def calculate_average(data_array):
+        array_sum = sum(data_array)
+        array_length = len(data_array)
+        return array_sum / array_length
+
+
+if __name__ == '__main__':
+    parameters = [7, 2, 1, 6, 3, 9, 5, 4, 25]
+    # result = 5.0
+    print(CalculateLocationParameters.calculate_median(parameters))
+    # result 6.89
+    print(CalculateLocationParameters.calculate_average(parameters))
+
+

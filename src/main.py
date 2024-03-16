d8814c255747bcd23dc684898715ba94ec49c461
l.knippenberg <lukas.knippenberg@noz-digital.de>
2024-03-16 18:40:05 +0100

    implement max_distance

--- src/main.py
+++ src/main.py
@@ -6,6 +6,7 @@ if __name__ == "__main__":
     path = "../dataset"
     analyzer = file_reader_h5.HDF5Analyzer(path)
     dataset = analyzer.handle_file_reader()
+    dataset_with_full_distance = analyzer.handle_set_full_distance(dataset)
 """
     mongoConnection.send_data_to_mongo(dataset)
 """
@@ -17,11 +18,13 @@ if __name__ == "__main__":
     print(query_content)
 """
 """So kann man z.B. die namen der datensätze ermitteln bei denen dein query zu trifft."""
+"""
 query_content = mongoConnection.count_data_from_mongo({
     "region": {"$in": ["Europe"]},
     "instrument": {"$in": ["Elephant"]}
 })
 print(query_content)
+"""
 """
 for data in query_content:
     print(data['file_name'])

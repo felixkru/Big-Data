0da13c3c840be071ce47f386b18b11803cf57ce3
l.knippenberg <lukas.knippenberg@noz-digital.de>
2024-03-17 19:55:10 +0100

    fix easteregg = 0

--- src/main.py
+++ src/main.py
@@ -1,30 +1,27 @@
 import file_reader_h5
 import check_data
-#import mongoConnection
+import mongoConnection
 from calculate_location_parameters import CalculateLocationParameters
 
 
 def handle_file_reader_and_write_to_database():
-    path = "../test"
+    path = "../dataset"
     collection = "complete_dataset"
     analyzer = file_reader_h5.HDF5Analyzer(path)
     dataset = analyzer.handle_file_reader()
 
-    # Bei dem dataset_with_velocity gibt es noch Probleme durch die Umwandulng des Timestamps
     dataset_with_velocity = check_data.CheckData.calculate_velocity_from_time_and_distance(dataset)
-
-    # Bei der Berechnung der statistischen Werte gibt es noch Probleme durch die falschen Timstamps
     dataset_with_statistic_parameters = CalculateLocationParameters.handle_update_average_and_median_calculation(dataset_with_velocity)
     full_dataset = analyzer.handle_set_full_distance(dataset_with_statistic_parameters)
 
-    #mongoConnection.send_data_to_mongo(full_dataset, collection)
+    mongoConnection.send_data_to_mongo(full_dataset, collection)
 
 
 if __name__ == "__main__":
     """
     Die Funktion verarbeitet das komplette Datenset
     """
-    handle_file_reader_and_write_to_database()
+    #handle_file_reader_and_write_to_database()
 
 """
     query_counter_result = mongoConnection.count_data_from_mongo({"region": "Africa"})
